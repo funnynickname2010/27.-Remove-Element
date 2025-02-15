dea0d6b2ed61d4c86383e4a447e4cf36d0c167d4
funnynickname2010 <rsramzesslava@gmail.com>
2025-02-15 20:11:48 +0300

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,64 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+/*The function takes as input a vector and an index.
+The value of the indexed position is deleted and the remaining
+values are shifted in-place of the removed value*/
+void FillInPosition(std::vector<int>& vec, size_t index);
+
+class Solution {
+public:
+    int removeElement(std::vector<int>& nums, int val) {
+        size_t element_counter = 0;
+        for (size_t i = 0; i < nums.size(); i++) {
+            if (nums[i] == val) {
+                FillInPosition(nums, i);
+            }
+            else {
+                element_counter++;
+            }
+        }
+
+        return element_counter;
+    }
+};
+
+void FillInPosition(std::vector<int>& vec, size_t index)
+{
+    for (size_t i = index; i < vec.size() - 1; i++) {
+        //Shifting the values by one to the left
+        vec[i] = vec[i + 1];
+    }
+}
+
+//TESTING------------------------------------------
+
+class TestVector : public ::testing::Test {
+protected:
+    std::vector<int> nums{ 0, 1, 1, 1, 2, 3, 4, 5 };
+    std::vector<int> nums_answer{ 0, 1, 1, 3, 4, 5 };
+    std::vector<int> nums_answer_2{ 0, 2, 3, 4, 5 };
+    int val;
+    int index;
+    Solution sol;
+
+    void SetUp() override {
+        val = 1;
+        index = 2;
+    }
+};
+
+TEST_F(TestVector, fill_in_position) {
+    FillInPosition(nums, index);
+    EXPECT_EQ(nums, nums_answer);
+}
+
+TEST_F(TestVector, remove_element) {
+    sol.removeElement(nums, val);
+    EXPECT_EQ(nums, nums_answer_2);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
